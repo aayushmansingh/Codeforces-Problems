ab9a6b7a1fab1e58401be314518d07a67dedb23e
Aayushman Singh <ayushmaan908@gmail.com>
2021-05-05 00:13:04 +0530

    First commit

---
+++ A_Amusing_Joke.py
@@ -0,0 +1,24 @@
+guest=input()
+host=input()
+result=input()
+freq={}
+for i in guest:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+for i in host:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+freqres={}
+for i in result:
+    if i in freqres:
+        freqres[i]+=1
+    else:
+        freqres[i]=1
+if freq==freqres:
+    print("YES")
+else:
+    print("NO")
