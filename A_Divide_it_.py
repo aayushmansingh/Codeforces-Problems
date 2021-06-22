8111709634209bd3a1e5c86f5fa94eef876d2a33
Aayushman Singh <63569767+aayushmansingh@users.noreply.github.com>
2021-06-22 11:31:49 +0530

    Add files via upload

---
+++ A_Divide_it_.py
@@ -0,0 +1,17 @@
+q=int(input())
+for i in range(q):
+    cnt2,cnt3,cnt5=0,0,0
+    n=int(input())
+    while(n%2==0):
+        n//=2
+        cnt2+=1
+    while(n%3==0):
+        n//=3
+        cnt3+=1
+    while(n%5==0):
+        n//=5
+        cnt5+=1
+    if n!=1:
+        print(-1)
+    else:
+        print(cnt2+2*cnt3+3*cnt5)
